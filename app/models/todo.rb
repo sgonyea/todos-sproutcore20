8616b748ec0d38e30e70469ee53b1b9ed868c451
Scott Gonyea <sgonyea@truecar.com>
2011-08-30 10:01:34 -0700

    virtual camelized attributes. Add a default scope; deleting actually only clears a Todo and hides it.

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,15 @@
 class Todo < ActiveRecord::Base
+  default_scope where(:is_cleared => false)
+
+  def isDone
+    is_done
+  end
+
+  def isDone=(isDone)
+    self.is_done = isDone
+  end
+
+  def as_json(*args)
+    { :id => id, :title => title, :isDone => is_done? }
+  end
 end
